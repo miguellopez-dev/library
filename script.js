5a36239223bc3f489b135780cfb25a41607e9e6b
Miguel Lopez <hello@miguellopez.dev>
2022-02-04 08:11:47 -0800

    Javascript so the user can input the information and it be stored in an object

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ let card = document.querySelector('.book__pop');
 let pop = document.querySelector('.book__pop');
 const form = document.getElementById('book-form');
 
+let myLibrary = [];
+
 // When a user clicks on the add button
 // Show a pop up with a form
 const open = function () {
@@ -22,8 +24,32 @@ window.onclick = function (event) {
 		form.reset();
 	}
 };
+
 // User will enter 'Title' 'Author' 'Pages' and whether they have read the book
+const btn = document.getElementById('btn-info');
+let submitInfo = function (form) {
+	let book1 = new Book(
+		form.title.value,
+		form.author.value,
+		form.pages.value,
+		form.read.value
+	);
+	console.log(book1);
+};
+btn.addEventListener('click', function () {
+	submitInfo(form);
+});
 // Information entered will be stored in an object
+function Book(title, author, pages, read) {
+	this.title = title;
+	this.author = author;
+	this.pages = pages;
+	this.read = read;
+}
 // the object will be stored in an array
 // the information bar will be updated with the details the user entered
 // A card will appear showing the information the user entered
+
+function addBookToLibrary() {
+	// do stuff here
+}
